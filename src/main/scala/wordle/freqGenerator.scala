7b5531d0ca1eeaf654de96003ff13836b0d1d76d
Matthew Sharp <sharpshopter@gmail.com>
2022-04-30 17:31:32 +1000

    Remove unused app

--- src/main/scala/wordle/freqGenerator.scala
+++
@@ -1,8 +0,0 @@
-package wordle
-
-import java.io.{FileOutputStream, ObjectOutputStream}
-import scala.io.Source
-
-object freqGenerator extends App {
-  val words = Source.fromFile("wordlist").getLines().toList
-}
