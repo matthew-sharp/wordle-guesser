137555ecd4f2487fbcd5f40d13762abe22a12613
Matthew Sharp <sharpshopter@gmail.com>
2022-05-01 15:31:32 +1000

    Modify guesser to prefer unique letters in early guesses

--- src/main/scala/wordle/WordleGuesser.scala
+++ src/main/scala/wordle/WordleGuesser.scala
@@ -4,9 +4,8 @@ class WordleGuesser(words: Set[String], resultCallback: String => List[Constrain
   val rows = 5
   val cols = 26
 
-  var currentlyValidWords: Set[String] = words
-
-  var cons: List[Constraint] = List[Constraint]()
+  private var currentlyValidWords: Set[String] = words
+  private var cons: List[Constraint] = List[Constraint]()
 
   def guess(): (Int, String) = {
     var candidateWord = ""
@@ -16,11 +15,22 @@ class WordleGuesser(words: Set[String], resultCallback: String => List[Constrain
       println(s"${currentlyValidWords.size} possible words")
       print("selecting candidate: ")
       val freqTable = FrequencyCalculator.calc(currentlyValidWords)
-      candidateWord = currentlyValidWords.maxBy(WordScorer.geomeanScorer(_, freqTable))
+      candidateWord = currentlyValidWords.maxBy { candidate =>
+          WordScorer.geomeanScorer(candidate, freqTable) * avoidDoubleFactor(guessNum, candidate)
+      }
       println(candidateWord)
       cons = resultCallback(candidateWord)
       currentlyValidWords = WordPruner.pruneWords(currentlyValidWords, cons)
     } while (!cons.forall(_.constraintType == ConstraintType.Position))
     (guessNum, candidateWord)
+  }
+
+  def avoidDoubleFactor(guessNum: Int, candidate: String): Double = {
+    val guessFactor = Array[Double](2, 1.75, 1.5, 1.25, 1, 1, 1, 1, 1, 1).apply(guessNum - 1)
+    val avoidFactor = if(guessFactor < 1.01) Math.E else {
+      val uniqueLettersInWord = candidate.toSet.size
+      uniqueLettersInWord * guessFactor
+    }
+    Math.log(avoidFactor)
   }
 }
