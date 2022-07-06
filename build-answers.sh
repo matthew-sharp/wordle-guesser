703ff89c9b0ab74ac8dcd7cc87627e93922af111
Matthew Sharp <sharpshopter@gmail.com>
2022-07-06 21:03:54 +1000

    Add slightly trimmed answer list (remove plurals and "ed"s)

--- build-answers.sh
+++ build-answers.sh
@@ -2,3 +2,23 @@
 
 set -eo pipefail
 
+proj_dir=~/dev/wordle/
+cd "$(mktemp -d)"
+
+# Eliminate plurals
+grep '^....$' /usr/share/dict/words | \
+grep -v chao | \
+grep -v gros | \
+grep -v toru | \
+sed -e 's/$/s/' | sort > plurals
+
+comm -2 -3 $proj_dir/src/main/resources/wordlist plurals > sans-plurals
+
+# Eliminate "ed" suffix
+grep '^...$' /usr/share/dict/words > 3letters
+grep -v bus < 3letters | \
+grep -v shi | \
+grep -v tri | \
+sed -e 's/$/ed/' | sort > eds
+
+comm -2 -3 sans-plurals eds
--- src/main/scala/wordle/io/AnswerListReader.scala
+++ src/main/scala/wordle/io/AnswerListReader.scala
@@ -8,9 +8,18 @@ import scala.io.Source
 
 object AnswerListReader {
   def read(filename: String = "answer-list"): IO[Seq[String]] = {
-    val file = Resource.fromAutoCloseable(IO.blocking(Source.fromFile(filename)))
-    file.use ( source =>
-      IO(source.getLines().toSeq)
-    )
+    val fileExists = IO.blocking(Files.exists(Paths.get(filename)))
+    val wordsSource = fileExists.flatMap { exists =>
+      if (exists)
+        IO(Resource.fromAutoCloseable(IO.blocking(Source.fromFile(filename))))
+      else {
+        IO.println("answer-list not found in current directory, using built-in list") >>
+          IO(Resource.fromAutoCloseable(IO.blocking(Source.fromResource("answer-list"))))
+      }
+    }
+    wordsSource.flatMap(resource =>
+      resource.use(source =>
+        IO(source.getLines().toSeq)
+      ))
   }
 }
