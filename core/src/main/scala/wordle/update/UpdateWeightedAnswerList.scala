bbaf30e6d2652b98b6a162819bfdd7a8f1aa18a3
Matthew Sharp <sharpshopter@gmail.com>
2022-07-15 08:52:04 +1000

    feat: Support tab as well as space delimited weighted answer lists

--- core/src/main/scala/wordle/update/UpdateWeightedAnswerList.scala
+++ core/src/main/scala/wordle/update/UpdateWeightedAnswerList.scala
@@ -5,7 +5,7 @@ import wordle.model.{Model, setOutputMsg}
 
 object UpdateWeightedAnswerList {
   def apply(model: Model, lines: Seq[String]): (Model, Cmd) = {
-    val weightMap = lines.map(_.split(" +"))
+    val weightMap = lines.map(_.split("[\t ]+"))
       .map(w => (model.resultsCache.reverseWordMapping(w(0)), w(1).toDouble))
       .toMap
     (model.setOutputMsg(s"${lines.size} answers read")
