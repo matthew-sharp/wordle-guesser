e9c80efb1c35a69efcd299e6c6d62efd340c7081
Matthew Sharp <sharpshopter@gmail.com>
2022-07-15 21:39:40 +1000

    fix: Start auto solve no longer crashes on unknown word

--- core/src/main/scala/wordle/auto/StartAutoSolve.scala
+++ core/src/main/scala/wordle/auto/StartAutoSolve.scala
@@ -2,17 +2,20 @@ package wordle.auto
 
 import wordle.Cmd
 import wordle.entropy.EntropyScorer
-import wordle.model.Model
+import wordle.model.{Model, setOutputMsg}
 import wordle.update.StartSolveCommon
 import wordle.util.LookupPruner
 
 object StartAutoSolve {
   def apply(model: Model, answer: String): (Model, Cmd) = {
-    val solver = AutoSolver(
-      answer = model.resultsCache.reverseWordMapping(answer),
-      scorer = EntropyScorer(model.resultsCache),
-      pruner = LookupPruner(model.resultsCache),
-    )
-    StartSolveCommon(model, solver, 1)
+    val ansIdx = model.resultsCache.reverseWordMapping.get(answer)
+    ansIdx.map { answer =>
+      AutoSolver(
+        answer = answer,
+        scorer = EntropyScorer(model.resultsCache),
+        pruner = LookupPruner(model.resultsCache),
+      )
+    }.map(solver => StartSolveCommon(model, solver, 1))
+      .getOrElse((model.setOutputMsg(s"$answer not in master wordlist"), Cmd.Nothing))
   }
 }
