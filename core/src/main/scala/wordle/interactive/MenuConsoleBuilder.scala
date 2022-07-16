6b236bd9b30abc2ddf330409ce590b11e1aa5817
Matthew Sharp <sharpshopter@gmail.com>
2022-07-16 20:09:44 +1000

    feat: Interactive menu exposes more information.

--- core/src/main/scala/wordle/interactive/MenuConsoleBuilder.scala
+++ core/src/main/scala/wordle/interactive/MenuConsoleBuilder.scala
@@ -8,10 +8,19 @@ import scala.collection.mutable
 
 object MenuConsoleBuilder {
   def apply(model: Model, guessScoreInfo: Map[Word, Seq[ScoreInfo]]): (Model, Cmd) = {
-    def menuPrompt(menu: Seq[((Word, Double, String), Int)]): String = {
+    def menuPrompt(menu: Seq[((Word, Seq[ScoreInfo], String), Int)]): String = {
       val sb = mutable.StringBuilder(s"${menu.size} best words:\n")
+      sb ++= "\t\t\test guesses\tP(word)\tE(word)\n"
       menu.foreach(i =>
-        sb ++= s"${i._2}.\t${model.resultsCache.wordMapping(i._1._1)}${i._1._3}\t${i._1._2}\n"
+        val scoreInfo = i._1._2
+        val totalScore = scoreInfo.map(_.score).sum
+        val infoSuffix = if scoreInfo.size >1 then
+          f"$totalScore%.6f"
+        else {
+          val si = scoreInfo.head
+          f"$totalScore%.6f\t${si.probability*100}%.2f%%\t${si.rawScore}%.6f"
+        }
+        sb ++= s"${i._2}.\t${model.resultsCache.wordMapping(i._1._1)}${i._1._3}\t$infoSuffix\n"
       )
       sb ++= "Enter number from menu or type guess word"
       sb.toString
@@ -20,7 +29,7 @@ object MenuConsoleBuilder {
     val guessScore = guessScoreInfo.view.mapValues(_.map(_.score).sum).toMap
     val topWords = TopN(guessScore, 10).takeWhile(guessScore(_) > 0)
     val menu = topWords.map(w => (
-      w, guessScore(w),
+      w, guessScoreInfo(w),
       if (model.boards.size == 1 && !model.boards.head.currentlyPossibleAnswers.contains(w)) "*" else "")).zipWithIndex
     val inputMapping = menu.map(i => i._2 -> i._1._1).toMap
     val console = Console(
