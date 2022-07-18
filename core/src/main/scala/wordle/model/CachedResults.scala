28e64bfa2dea02dc120faca28cef65a0ed29fb4b
Matthew Sharp <sharpshopter@gmail.com>
2022-07-18 18:38:16 +1000

    refactor: Add safer access to result cache slice

--- core/src/main/scala/wordle/model/CachedResults.scala
+++ core/src/main/scala/wordle/model/CachedResults.scala
@@ -1,13 +1,18 @@
 package wordle.model
 
 case class CachedResults(
-                          resultLookup: IArray[ResultTernary],
+                          private val resultLookup: IArray[ResultTernary],
                           wordMapping: IndexedSeq[String],
                           reverseWordMapping: Map[String, Word],
                         )
 {
   private val arrayDim = wordMapping.size
+
   inline def getResult(guess: Word, answer: Word): ResultTernary = {
     resultLookup(guess * arrayDim + answer)
+  }
+
+  inline def getResultsSlice(guess: Word): IArray[ResultTernary] = {
+    resultLookup.slice(guess * arrayDim, guess * arrayDim + arrayDim)
   }
 }
--- core/src/main/scala/wordle/util/LookupPruner.scala
+++ core/src/main/scala/wordle/util/LookupPruner.scala
@@ -4,10 +4,9 @@ import wordle.model.*
 
 import scala.collection.immutable.BitSet
 
-class LookupPruner(cache: CachedResults) extends Pruner {
+case class LookupPruner(cache: CachedResults) extends Pruner {
   def pruneWords(words: BitSet, constraints: ResultTernary, guess: Word): BitSet = {
-    val totalWords = cache.wordMapping.size
-    val cacheSlice = cache.resultLookup.slice(guess * totalWords, guess * totalWords + totalWords)
+    val cacheSlice = cache.getResultsSlice(guess)
     BitSet.fromSpecific(cacheSlice.zipWithIndex.filter(_._1 == constraints).map(_._2)).intersect(words)
   }
 }
